02cd741d82649b10ab0f37829bf956c98808f131
CSE2023786 <cao.fengning.68w@st.kyoto-u.ac.jp>
2022-07-12 20:51:18 +0900

    test

---
+++ src/components/Footer.js
@@ -0,0 +1,42 @@
+import React from "react";
+import { FaFacebook,FaLinkedinIn,FaMailBulk,FaPhone,FaSearchLocation,FaTwitter } from "react-icons/fa";
+import './FooterStyles.css';
+
+
+const Footer = () => {
+    return(
+        <div className='footer'>
+            <div className='footer-container'>
+                <div className='left'>
+                    <div className='location'>
+                        <FaSearchLocation size={20} style={{color:'#ffffff',marginRight:'2rem'}} />
+                        <div>
+                        <p>Our street 123 Acme St.</p>
+                        <h4>Chongqing.CN</h4>
+                        </div>
+                    </div>
+                    <div className='phone'>
+                        <h4><FaPhone size={20} style={{color:'#ffffff',marginRight:'2rem'}} /> 1-234-5678</h4>
+                    </div> 
+                    <div className='email'>
+                        <h4><FaMailBulk size={20} style={{color:'#ffffff',marginRight:'2rem'}} /> 1-234-5678</h4><h4> trips@gamil.com</h4>
+                    </div>
+                </div>
+                <div className='right'>
+                    <h4>About the company</h4>
+                    <p>this is a good site</p>
+                    <div className='social'>
+                        <FaFacebook size={30} style={{color:'#ffffff',marginRight:'1rem'}}/>
+                        <FaSearchLocation size={30} style={{color:'#ffffff',marginRight:'1rem'}}/>
+                        <FaLinkedinIn size={30} style={{color:'#ffffff',marginRight:'1rem'}}/>
+
+                    </div>
+                </div>
+            </div>
+        
+        </div>
+    )
+
+}
+
+export default Footer
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,39 +1,46 @@
-import React,{useState} from 'react';
-import {Link} from 'react-router-dom';
-import {FaBars, FaTimes} from 'react-icons/fa';
-import './NavbarStyles.css';
+import React, {useState} from 'react'
+import {Link} from 'react-router-dom'
+import {FaBars, FaTimes} from 'react-icons/fa'
+import './NavbarStyles.css'
 
 const Navbar = () => {
-  const[click,setClick] = useState(false)
-  const handleClick = () => setClick(!click)
+    const[click, setClick] = useState(false)
+    const handleClick = () => setClick(!click)
 
+    const [color, setColor] = useState(false)
+        const changeColor =() => {
+            if(window.scrollY >= 100) {
+                setColor(true)
+            } else {
+                setColor(false)
+            }
+        }
 
-  return (
-    <div className='header'>
-        <Link to='/'><h1>GLX TRVL</h1></Link>
-        <ul className={click ? 'nav-menu active' : 'nav-menu'}>
-            <li>
-            <Link to='/'>Home</Link>
-            </li>
-            <li>
-            <Link to='/pricing'>pricing</Link>
-            </li>
-            <li>
-            <Link to='/trainning'>Trainning</Link>
-            </li>
-            <li>
-            <Link to='/contact'>Contact</Link>
-            </li>
+        window.addEventListener('scroll', changeColor)
 
-        </ul>
-
-    <div className='hamburger' onClick={(handleClick)}>
-      {click ? (<FaTimes size={20} style={{color:'#fff'}} />) : (<FaBars size={20} style={{color:'#fff'}} />)}
-
-        <FaBars size={20} style={{color:'#fff'}}/>
+    return (
+        <div className={color ? 'header header-bg' : 'header'}>
+           <Link to='/'><h1>GLX TRVL</h1></Link> 
+           <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+               <li>
+                   <Link to='/'>Home</Link>
+               </li>
+               <li>
+                   <Link to='/pricing'>Pricing</Link>
+               </li>
+               <li>
+                   <Link to='/training'>Training</Link>
+               </li>
+               <li>
+                   <Link to='/contact'>Contact</Link>
+               </li>
+           </ul>
+           <div className='hamburger' onClick={handleClick}>
+            {click ? (<FaTimes size={20} style={{color: '#fff'}} />) : (<FaBars size={20} style={{color: '#fff'}} />)}
+               
+           </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Navbar
